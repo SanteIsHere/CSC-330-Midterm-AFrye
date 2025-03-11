8efd5aa7ef80f509557fc53271348367312a1352
SanteIsHere <r.a.frye00@gmail.com>
2025-03-10 21:29:45 -0400

    Initialize feature branch for calculator application; Create `app.py`, title document, import flask dependencies.

---
+++ src/app.py
@@ -0,0 +1,9 @@
+"""
+Name: app.py
+Author: Asante Frye
+Description: Flask application simulating a calculator
+that can perform basic arithmetic operations with URL parameters.
+"""
+
+from flask import Flask, render_template
+
